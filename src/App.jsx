d394197268d7fd837676f0bdd91b1b81eddc9009
freki-inc <freki.inc@outlook.com>
2026-07-25 19:48:22 +0200

    Fix image impors for production

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,22 @@ import { Contact } from './components/Contact'
 import { Footer } from './components/Footer'
 import './App.css'
 
+import wof1 from './assets/images/wof1.webp'
+import wof2 from './assets/images/wof2.webp'
+import wof3 from './assets/images/wof3.webp'
+import wof4 from './assets/images/wof4.webp'
+import wof5 from './assets/images/wof5.webp'
+
 function App() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const images = [
-    { url: '../src/assets/images/wof1.WebP', alt: 'geslaagde leerling' },
-    { url: '../src/assets/images/wof2.WebP', alt: 'geslaagde leerling' },
-    { url: '../src/assets/images/wof3.WebP', alt: 'geslaagde leerling' },
-    { url: '../src/assets/images/wof4.WebP', alt: 'geslaagde leerling' },
-    { url: '../src/assets/images/wof5.WebP', alt: 'geslaagde leerling' },
-  ];
+    { url: wof1, alt: 'geslaagde leerling' },
+    { url: wof2, alt: 'geslaagde leerling' },
+    { url: wof3, alt: 'geslaagde leerling' },
+    { url: wof4, alt: 'geslaagde leerling' },
+    { url: wof5, alt: 'geslaagde leerling' },
+  ]
 
   return (
     <main>
