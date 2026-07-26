5e2cef13e4ca03236c15329f368bae4ba012e301
freki-inc <freki.inc@outlook.com>
2026-07-26 13:19:40 +0200

    src fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 export const Hero = () => {
     return (
         <div className="hero-section flex-column">
-            <img src="../assets/images/hero-img.webp" fetchpriority="high" className="hero-img" title="Wiebe’s Rijschool" alt="rijinstructeur Wiebe"></img>
-            <img className="logo-site-links on-top" src="../assets/images/logo.webp" alt="Wiebe’s Autorijschool"></img>
+            <img src="./src/assets/images/hero-img.webp" fetchpriority="high" className="hero-img" title="Wiebe’s Rijschool" alt="rijinstructeur Wiebe"></img>
+            <img className="logo-site-links on-top" src="./src/assets/images/logo.webp" alt="Wiebe’s Autorijschool"></img>
             {/* <h1 className="hero-title on-top">Wiebe’s Autorijschool</h1> */}
             <h2 className="hero-quote on-top mb-1">Autorijden geeft je vrijheid</h2>
             <button className="hero-button on-top">Proefles aanvragen</button>
